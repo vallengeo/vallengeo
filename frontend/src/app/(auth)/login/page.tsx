138752796dfcdb3f46583b2bea1b3348d5e662ac
Luan Henrique <vallengeo.dev@gmail.com>
2023-10-22 18:12:06 -0300

    feat(login/forgotpassword): inserindo campos no formulario

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,17 +1,40 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
 import Support from "@/components/support";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   return (
-    <>
+    <div>
       <p className="mt-8 mb-6">Acesse a plataforma de regularização de imóveis através dos campos abaixo.</p>
 
       <form action="" className="flex flex-col">
+        <div className="flex flex-col gap-2 mb-2">
+          <Label htmlFor="email">E-mail</Label>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="digite um e-mail"
+            required
+          />
+        </div>
 
-        {/* todo: Campo de entrada */}
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="password">Senha</Label>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="********"
+            min="6"
+            required
+          />
+        </div>
 
-        <Button variant="link" className="mt-4 mb-6 justify-end px-0">
+        <Button variant="link" className="w-fit ml-auto mt-4 mb-6 justify-end px-0">
           <Link href="/forgotpassword">Esqueci minha senha</Link>
         </Button>
 
@@ -23,6 +46,6 @@ export default function LoginPage() {
           </Button>
         </div>
       </form>
-    </>
+    </div>
   )
 }
