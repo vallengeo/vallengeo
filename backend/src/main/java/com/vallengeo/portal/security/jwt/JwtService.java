95baa974a0a9c34bd289bb110cb337c41ecb92bd
Paulo Barbosa <paulohjbarbosa@gmail.com>
2024-01-07 18:26:15 -0300

    chore: removendo arquivos não utilizados

--- backend/src/main/java/com/vallengeo/portal/security/jwt/JwtService.java
+++ backend/src/main/java/com/vallengeo/portal/security/jwt/JwtService.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
