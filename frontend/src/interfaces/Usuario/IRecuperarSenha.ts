cc7da01021b92b199587d06c05901f30bfb5ca48
code-luanhs <code.luanhs@gmail.com>
2025-01-07 22:04:48 -0300

    feat: criando chamada das operacoes

---
+++ frontend/src/interfaces/Usuario/IRecuperarSenha.ts
@@ -0,0 +1,4 @@
+export default interface IRecuperarSenha {
+  senha: string;
+  codigoAcesso: string;
+}
--- frontend/src/service/analista/analistaService.ts
+++ frontend/src/service/analista/analistaService.ts
@@ -1,6 +1,29 @@
 import api from "@/service/api";
 import Cookies from "js-cookie";
 import { ACCESS_TOKEN } from "@/constants/auth";
+import IDownloadRelatorio from "@/interfaces/Analista/IDownloadRelatorio";
+import IObservacaoProtocolo from "@/interfaces/Analista/IObservacaoProtocolo";
+import IArquivarImovel from "@/interfaces/Analista/IArquivarImovel";
+
+export const filtrosRelatorio = async () => {
+  const token = Cookies.get(ACCESS_TOKEN);
+
+  return await api.get("analista/relatorio/filtro", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const protocolo = async (processoId: string) => {
+  const token = Cookies.get(ACCESS_TOKEN);
+
+  return await api.get(`analista/protocolo/${processoId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 
 export const notificacaoNaoVisualizada = async () => {
   const token = Cookies.get(ACCESS_TOKEN);
@@ -68,6 +91,18 @@ export const ultimosAlterados = async () => {
   });
 };
 
+export const ficha = async (processoId: string) => {
+  const token = Cookies.get(ACCESS_TOKEN);
+
+  const response = await api.get(`analista/imovel/ficha/${processoId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
+
 export const imoveisCadastrados = async () => {
   const token = Cookies.get(ACCESS_TOKEN);
 
@@ -94,10 +129,34 @@ export const imoveisCadastradosMapa = async () => {
   });
 };
 
-export const ficha = async (processoId: string) => {
+export const downloadRelatorio = async (formData: IDownloadRelatorio) => {
   const token = Cookies.get(ACCESS_TOKEN);
 
-  const response = await api.get(`analista/imovel/ficha/${processoId}`, {
+  const response = await api.post("analista/relatorio/download", formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
+
+export const observacaoProtocolo = async (formData: IObservacaoProtocolo) => {
+  const token = Cookies.get(ACCESS_TOKEN);
+
+  const response = await api.post("analista/protocolo/observacao", formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
+
+export const arquivarImovel = async (formData: IArquivarImovel) => {
+  const token = Cookies.get(ACCESS_TOKEN);
+
+  const response = await api.post("analista/imovel/arquivar", formData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
