f79b0965b9b3092ad5dbf4b8d7b02a7c657d621a
code-luanhs <code.luanhs@gmail.com>
2025-01-16 22:31:53 -0300

    style(brasao): add escrito prefeitura

--- frontend/src/components/brasao.tsx
+++ frontend/src/components/brasao.tsx
@@ -1,18 +1,22 @@
-import Image from 'next/image'
-import brasao from '@/assets/images/prefeitura/brasao_cruzeiro.webp'
+import Image from "next/image";
+import brasao from "@/assets/images/prefeitura/brasao_cruzeiro.webp";
+import { cn } from "@/lib/utils";
 
 interface BrasaoProps {
   className?: string;
 }
 
-export function Brasao({
-  className
-}: BrasaoProps) {
+export function Brasao({ className }: BrasaoProps) {
   return (
-    <Image
-      src={brasao}
-      alt="Brasão da Prefeitura"
-      className={className}
-    />
-  )
+    <div className={`flex items-center select-none ${cn(className, 'justify-center')}`}>
+      <Image
+        src={brasao}
+        alt="Brasão Cruzeiro"
+      />
+      <div className="flex flex-col text-[#1d1d1b] leading-[1] text-center">
+        <span className="font-light text-sm">Prefeitura de</span>
+        <span className="uppercase font-bold">CRUZEIRO</span>
+      </div>
+    </div>
+  );
 }
