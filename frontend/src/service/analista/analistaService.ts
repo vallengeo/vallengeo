bf782871c0a3b7270f64c1648427a63ad304baf0
code-luanhs <code.luanhs@gmail.com>
2025-01-21 21:50:15 -0300

    feat(analista): notificacoes > marcando como lida
    chore(analista): ficha > link para protocolo
    
    fix(cliente): reload para evitar cache ao deslogar da plataforma

--- frontend/src/service/analista/analistaService.ts
+++ frontend/src/service/analista/analistaService.ts
@@ -44,15 +44,17 @@ export const notificacaoNaoVisualizada = async () => {
 export const notificacaoVisualizada = async (notificacaoId: number) => {
   const token = Cookies.get(ACCESS_TOKEN);
 
-  return await api.post(
-    "analista/notificacao-visualizada",
-    notificacaoId,
+  const response = await api.post(
+    `analista/notificacao-visualizada/${notificacaoId}`,
+    {},
     {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiOTliNTUxZjgtZjUzMi00MGEwLWI2M2YtMDk0NjYxODQ0YmQ4Iiwibm9tZSI6InZhbGxlbmdlby5kZXZAZ21haWwuY29tIiwicHJpbWVpcm9Ob21lIjoidmFsbGVuZ2VvLmRldkBnbWFpbC5jb20iLCJlbWFpbCI6InZhbGxlbmdlby5kZXZAZ21haWwuY29tIiwiYXRpdm8iOnRydWUsImlkR3J1cG8iOiI0ZDNjMTQ5Ny1hZjQwLTRkZGYtOGIwNi1kOGY0MGM4ZGYxMzkiLCJncnVwb3MiOlsiQ1JVWkVJUk8iXSwidGVsYXMiOlsiSE9NRSIsIklNT1ZFTCIsIlBST1RPQ09MTyIsIlJFTEFUT1JJTyJdLCJwZXJmaXMiOlsiQURNSU5JU1RSQURPUiJdLCJwZXJtaXNzb2VzIjpbIkhPTUVfQVRVQUxJWkFDQU9fUFJPQ0VTU09fVklTVUFMSVpBUiIsIkhPTUVfUkVTVU1PX0lNT1ZFTF9BUlFVSVZBUiIsIkhPTUVfUkVTVU1PX0lNT1ZFTF9ET1dOTE9BRCIsIkhPTUVfUkVTVU1PX0lNT1ZFTF9FRElUQVIiLCJIT01FX1JFU1VNT19JTU9WRUxfVklTVUFMSVpBUiIsIkhPTUVfVUxUSU1PX1BST0NFU1NPX1ZJU1VBTElaQVIiLCJJTU9WRUxfQ0FEQVNUUkFSIiwiSU1PVkVMX0xJU1RBX0lNT1ZFTF9BUlFVSVZBUiIsIklNT1ZFTF9MSVNUQV9JTU9WRUxfUkVMQVRPUklPIiwiSU1PVkVMX0xJU1RBX0lNT1ZFTF9WSVNVQUxJWkFSIiwiUFJPVE9DT0xPX0xJU1RBX1BST0NFU1NPX0FSUVVJVkFSIiwiUFJPVE9DT0xPX0xJU1RBX1BST0NFU1NPX1JFTEFUT1JJTyIsIlBST1RPQ09MT19MSVNUQV9QUk9DRVNTT19WSVNVQUxJWkFSIiwiUkVMQVRPUklPX0dFUkFSIiwiUkVMQVRPUklPX1JFU1VNT19JTU9WRUxfRE9XTkxPQUQiXX0sInN1YiI6InZhbGxlbmdlby5kZXZAZ21haWwuY29tIiwiaWF0IjoxNzM3NTA2MDk3LCJleHAiOjE3Mzc1OTI0OTd9.JXRvDsa8URnisjRwd_P6DMpl9Oz0HL6JdwL3gD42flM`,
       },
     }
   );
+
+  return response.data;
 };
 
 export const totalizadoresProcesso = async () => {
